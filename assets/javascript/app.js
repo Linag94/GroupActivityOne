35f77e2c5d43e91b00f5450dec6cbbb4dc7e072b
Thelina Geogre <George.thelina@gmail.com>
2019-09-04 20:26:06 -0400

    added css and skeleton plave holders

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// $.get("/yourURL")
+//     .then(function(result){
+//         for(var i = 0; i < result.length; i++){
+//             var youtTubeCard = $("<div class='youtTubeCard'>");
+//             var vidCard = $("<div class='vidCard'>");
+//             var youtubeVid = $("<img>")
+//                 youtubeVid.attr("src", result[i].url)
+//                 youtubeVid.attr("alt", "youTubeVideo");
+//             vidCard.append(youtubeVid);
+            
